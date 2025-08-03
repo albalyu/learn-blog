77c555df45c0b3ebdfe3ef7b3f4dd3f258ddafd0
albalyu <albalyu@gmail.com>
2025-08-03 20:38:43 +0300

    feat: Implement comments feature; add Comment, CommentForm, and CommentList components; create API routes for fetching and adding comments; update PostDetailsPage to display and manage comments

---
+++ client/src/components/CommentList.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Comment from './Comment';
+
+const CommentList = ({ comments }) => {
+  return (
+    <div>
+      {comments.map((comment) => (
+        <Comment key={comment.id} comment={comment} />
+      ))}
+    </div>
+  );
+};
+
+export default CommentList;
